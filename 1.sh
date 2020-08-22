f8d85ffa01c9032177b54cb0aef27a9e37f2800e
fLoveARTh <flovearth@protonmail.com>
2020-08-22 18:36:24 +0200

    first commit

--- 1.sh
+++ 1.sh
@@ -5,4 +5,4 @@ echo "Enter a number: "
 read number
 total=$((number+base_value))
 echo "Total value is: $total"
-1 day ago24 day ago47 day ago70 day ago93 day ago116 day ago139 day ago162 day ago185 day ago1 day ago11 day ago21 day ago31 day ago41 day ago51 day ago61 day ago
+1 day ago24 day ago47 day ago70 day ago93 day ago116 day ago139 day ago162 day ago185 day ago1 day ago11 day ago21 day ago31 day ago41 day ago51 day ago61 day ago71 day ago
