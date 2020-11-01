de0293ead0328561f41cbb5339498910e658fbbc
fLoveARTh <flovearth@protonmail.com>
2020-11-01 17:42:03 +0100

    exercise

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-s
+#!/bin/bash
+
+read -p "How old are you: " age
+
+if ((age >= 16 )) 
+then
+	echo "You can drive"
+elif [ $age -eq 15 ];
+then
+	echo "You can drive next year..."
+else
+	echo "You can't drive baby :-("
+fi
