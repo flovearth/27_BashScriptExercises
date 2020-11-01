cd83ebf288f77c656108bbd49f5c0cc9c3440b8a
fLoveARTh <flovearth@protonmail.com>
2020-11-01 17:38:29 +0100

    Working...

--- 1.sh
+++ 1.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-base_value=5
-echo "Enter a number: "
-read number
-total=$((number+base_value))
-echo "Total value is: $total"
-1 day ago24 day ago47 day ago70 day ago93 day ago116 day ago139 day ago162 day ago185 day ago1 day ago11 day ago21 day ago31 day ago41 day ago51 day ago61 day ago71 day ago81 day ago91 day ago101 day ago111 day ago121 day ago131 day ago141 day ago151 day ago161 day ago171 day ago181 day ago191 day ago
+read -p "Enter Number1: " n1
+read -p "Enter Number2: " n2
+read -p "Enter Number3: " n3
+
+if (( ((n2<n3)) && ((n2>n1)) ))
+then
+	echo "Number2 is between Number1 and Number3"
+elif  ((n2>n3)) 
+then
+	echo "Number2 is greater than Number3 "
+
+elif (( ((n2>n3)) || ((n2<n1)) ))
+then
+	echo "Out of range."
+
+fi
